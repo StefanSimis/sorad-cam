8217e276a54d5353435ad6e8fb46c36290b08468
stefan simis <stsi@pml.ac.uk>
2024-01-05 13:07:53 +0000

    add nginx setup. some css styling

--- main.py
+++ main.py
@@ -1,68 +1,60 @@
 from flask import Flask, render_template, Response, request, send_from_directory, redirect, url_for
 from picamera import Picamera
+import os
+import datetime
 
-camera = Picamera(hflip=0, vflip=1, res='full')
-app = Flask(__name__)
+try:
+    camera = Picamera(hflip=0, vflip=1, res='full')
+
+    app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
+    @app.route('/')
+    @app.route('/index')
+    def index():
+        try:
+            size = os.path.getsize('static/latest-image.jpg')
+            size = size/1024.
+            mtime = os.path.getmtime('static/latest-image.jpg')
+            mtime = datetime.datetime.fromtimestamp(mtime)
+        except Exception as msg:
+            size = 'no image found'
+            mtime = 'no image found'
+            print(msg)
+            print(os.listdir('.'))
+        return render_template('index.html', size=size, mtime=mtime)
 
-@app.route('/picture')
-def take_picture():
-    # take picture with current configuration
-    camera.take_picture()
-    return redirect(url_for('index'))
+    @app.route('/getpicture')
+    def get_picture():
+        # take picutre and send jpg with current configuration
+        camera.take_picture()
+        return send_from_directory(directory='static', filename='latest-image.jpg')
 
-@app.route('/getpicture')
-def get_picture():
-    # take picutre and send jpg with current configuration
-    camera.take_picture()
-    return send_from_directory(directory='static', filename='latest-image.jpg')
+    @app.route('/getfull')
+    def get_picture_full():
+        # configure as full res, take picture and send as jpeg
+        camera.configure(hflip=0, vflip=1, res='full')
+        camera.take_picture()
+        return send_from_directory(directory='static', filename='latest-image.jpg')
 
-@app.route('/full')
-def take_picture_full():
-    # configure as full res and take picture
-    camera.configure(hflip=0, vflip=1, res='full')
-    camera.take_picture()
-    return redirect(url_for('index'))
+    @app.route('/getlow')
+    def get_picture_low():
+        # configure as full res, take picture and send as jpeg
+        camera.configure(hflip=0, vflip=1, res='low')
+        camera.take_picture()
+        return send_from_directory(directory='static', filename='latest-image.jpg')
 
-@app.route('/getfull')
-def get_picture_full():
-    # configure as full res, take picture and send as jpeg
-    camera.configure(hflip=0, vflip=1, res='full')
-    camera.take_picture()
-    return send_from_directory(directory='static', filename='latest-image.jpg')
+    @app.route('/getmedium')
+    def get_picture_medium():
+        # configure as full res, take picture and send as jpeg
+        camera.configure(hflip=0, vflip=1, res='medium')
+        camera.take_picture()
+        return send_from_directory(directory='static', filename='latest-image.jpg')
 
-@app.route('/low')
-def take_picture_low():
-    # configure as full res and take picture
-    camera.configure(hflip=0, vflip=1, res='low')
-    camera.take_picture()
-    return redirect(url_for('index'))
-
-@app.route('/getlow')
-def get_picture_low():
-    # configure as full res, take picture and send as jpeg
-    camera.configure(hflip=0, vflip=1, res='low')
-    camera.take_picture()
-    return send_from_directory(directory='static', filename='latest-image.jpg')
-
-@app.route('/medium')
-def take_picture_medium():
-    # configure as full res and take picture
-    camera.configure(hflip=0, vflip=1, res='medium')
-    camera.take_picture()
-    return redirect(url_for('index'))
-
-@app.route('/getmedium')
-def get_picture_medium():
-    # configure as full res, take picture and send as jpeg
-    camera.configure(hflip=0, vflip=1, res='medium')
-    camera.take_picture()
-    return send_from_directory(directory='static', filename='latest-image.jpg')
+except RuntimeError:
+    raise
+except KeyboardInterrupt:
+    camera.close()
 
 
 if __name__ == '__main__':
